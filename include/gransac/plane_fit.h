f2ff1924efa05229d12b41098836c32080ce2516
Matthew Klein <gmattklein@gmail.com>
2018-06-11 16:01:51 -0400

    Use PCL for plane_fit

--- include/gransac/plane_fit.h
+++ include/gransac/plane_fit.h
@@ -1,4 +1,6 @@
 #include <opencv2/opencv.hpp>
+#include <pcl/point_types.h>
+#include <pcl/point_cloud.h>
 
 #include "gransac/GRANSAC.hpp"
 #include "gransac/PlaneModel.hpp"
@@ -12,11 +14,15 @@ class PlaneFit {
  public:
   PlaneFit();
   PlaneFit(const cv::Mat3d& data_points);
+  PlaneFit(const pcl::PointCloud<pcl::PointXYZ>& data_points);
   ~PlaneFit();
 
   void load_data_points(const cv::Mat3d& data_points);
+  void load_data_points(const pcl::PointCloud<pcl::PointXYZ>& data_points);
   void clear_data_points();
   void get_best_inliers(cv::Mat3d& best_inliers);
+  void get_best_inliers(pcl::PointCloud<pcl::PointXYZ>& best_inliers);
   bool get_best_model(cv::Mat3d& best_model);
+  bool get_best_model(pcl::PointCloud<pcl::PointXYZ>& best_model);
   bool estimate(GRANSAC::VPFloat threshold, int max_iterations = 1000);
 };
